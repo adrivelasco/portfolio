e70e8c5d9d967cf4bbdb4a8cbc2f4f6e480b2c45
adrivelasco <adrivelasco96@hotmail.com>
2018-05-08 12:22:28 -0300

    refactor: Remove enters

--- server/config/index.ts
+++ server/config/index.ts
@@ -7,12 +7,9 @@ interface IConfig {
 }
 
 const config: IConfig = {
-
   env: process.env.NODE_ENV || 'production',
   port: process.env.PORT || '3002',
-
   apiBasePath: '/',
-
 };
 
 export default config;
